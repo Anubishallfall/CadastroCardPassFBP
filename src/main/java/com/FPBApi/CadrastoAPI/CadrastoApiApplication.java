aca60b72ff9fe5f84dc51c2e3290323c0dc28994
Mauro Peixoto <maurograp24@gmail.com>
2022-06-03 10:48:34 -0300

    primeiro Commit

---
+++ src/main/java/com/FPBApi/CadrastoAPI/CadrastoApiApplication.java
@@ -0,0 +1,13 @@
+package com.FPBApi.CadrastoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadrastoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadrastoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FPBApi/CadrastoAPI/CadrastoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FPBApi.CadrastoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadrastoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
